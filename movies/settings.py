993fe398285a4b4ad9ec91cec73a910b9227bd4c
tylerschu1 <tyler.schu1@outlook.com>
2023-03-08 01:27:10 -0500

    Delete and 404

--- movies/settings.py
+++ movies/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-80+8gkc_g^fw_%dfg=gal!v1rdpgwic)72qt16ma_m2k8w$x+x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- movies/urls.py
+++ movies/urls.py
@@ -20,5 +20,8 @@ from movies import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
-    path('movies/',views.movies)
+    path('movies/',views.movies),
+    path('movies/<int:id>',views.detail),
+    path('movies/add', views.add),
+    path('movie/delete/<int:id>', views.delete)
 ]
